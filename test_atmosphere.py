e2ae04c267edcbdc4b47ac3461ec4540bab70f1a
Nestor Espinoza <nespinoza@stsci.edu>
2024-03-06 01:35:13 -0500

    resolution tests

--- test_atmosphere.py
+++ test_atmosphere.py
@@ -1,3 +1,4 @@
+import numpy as np
 import transitspectroscopy as ts
 import matplotlib.pyplot as plt
 import utils
@@ -18,6 +19,8 @@ planet_properties['T_eq'] = 255
 bulk_species = ['N2']
 param_species = ['O2', 'CO2', 'CH4', 'H2O', 'O3', 'N2O']
 
+w, d, derr = np.loadtxt('data/espinoza/visit1.txt', unpack = True, usecols = (0,1,2))
+
 # Test at R = 10,000
 atmosphere = utils.generate_atmosphere(star_properties, planet_properties, param_species, bulk_species)
 spectrum = atmosphere.get_spectrum()
@@ -31,11 +34,9 @@ atmosphere100 = utils.generate_atmosphere(star_properties, planet_properties, pa
 spectrum100 = atmosphere100.get_spectrum()
 
 # Bin all spectra at wavelengths of a dataset:
-w, d, derr = np.loadtxt('data/espinoza/visit1.txt', unpack = True, usecols = (0,1,2))
-
 plt.plot(w, utils.bin_to_data(w, atmosphere.wl, spectrum), '-', color = 'orangered', label = 'R = 10k, binned to data')
 plt.plot(w, utils.bin_to_data(w, atmosphere1k.wl, spectrum1k), '-', color = 'cornflowerblue', label = 'R = 1k, binned to data')
 plt.plot(w, utils.bin_to_data(w, atmosphere100.wl, spectrum100), '-', color = 'grey', label = 'R = 100, binned to data')
 
-plt.plot(atmosphere.wl, spectrum, '-')
+plt.legend()
 plt.show()
